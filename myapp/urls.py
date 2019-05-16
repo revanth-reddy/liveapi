7fba62989bcc85f24daddc073a51bb3ee53e9da2
revanth-reddy <malerevanthreddy3099@gmail.com>
2019-05-16 21:27:34 +0530

    static

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('temp', views.temp, name='temp'),
     path('test', views.test, name='test'),
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
